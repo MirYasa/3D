0c58e43a25119b86c22e4ee045dc3eb5316b614c
Miron Gurevich <gurevich.miron14@gmail.com>
2020-09-17 21:34:29 +0300

    feat (main.js) add event for images in command block, disabled letter input for calc block

--- script/main.js
+++ script/main.js
@@ -249,4 +249,44 @@ window.addEventListener('DOMContentLoaded', () => {
         startSlide();
     };
     slider();
+
+    const showImg = () => {
+        const command = document.querySelector('.command');
+
+        command.addEventListener(('mouseover'), (event) => {
+            const target = event.target;
+            let link;
+            if (target.classList.contains('command__photo')) {
+                link = target.src;
+                target.src = target.dataset.img;
+                target.dataset.img = link;
+            }
+        });
+
+        command.addEventListener(('mouseout'), (event) => {
+            const target = event.target;
+            let link;
+            if (target.classList.contains('command__photo')) {
+                link = target.src;
+                target.src = target.dataset.img;
+                target.dataset.img = link;
+            }
+        });
+    };
+    showImg();
+
+    const calculateCost = () => {
+        const calcBlock = document.querySelector('.calc-block');
+
+        calcBlock.addEventListener('input', (event) => {
+            const target = event.target;
+
+            if (target.classList.contains('calc-type')) {
+                return;
+            } else if (target.classList.contains('calc-item')) {
+                target.value = target.value.replace(/\D/g, '');
+            }
+        });
+    };
+    calculateCost();
 });
